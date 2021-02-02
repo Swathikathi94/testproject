de71a0b7afd71bf7575f3284e0587e61efcf73a8
SwathiK <SwathiK@SDI-HLSWATHIK.Hyderabad.smartdrive.net>
2021-02-02 22:06:36 +0530

    added main method

--- TestProject/src/Test.java
+++ TestProject/src/Test.java
@@ -1,13 +1,9 @@
 
 public class Test {
-	private void psvm() {
+	public static void main(String args[]) {
 		// TODO Auto-generated method stub
 		System.out.println("swathi");
 		System.out.println("");
 		System.out.println("dec");
-		
-		
-		
 	}
-	
 }
